84653a34113b56f7adcc9fea8e6a6237a248a5c3
Хантемир3 <kantemirkamcybekov70@gmail.com>
2023-10-27 17:05:57 +0300

    birinchi

---
+++ list.py
@@ -0,0 +1,11 @@
+# thislist = [1,4,6,2,3,45,23.9 ,"str" ,True ,False , 56j] 
+# print(thislist[3])
+
+
+
+str = ("banan")
+print(str)
+
+
+
+
---
+++ main.py
@@ -0,0 +1,246 @@
+#'' = одинарный
+#"" =  двойной
+#"""""" =   тройной
+
+
+
+
+#int 123345677
+#float = 23.424
+#"str "текст"
+
+
+
+
+
+#x = 1                      #int
+#y = 23456676867987          int
+#z = 4546657                # int
+
+
+
+#a=  """                          ёлка
+#       *
+#      * *
+#     * * *
+ #   * * * *
+ #  * * * * *
+ #     III"""     
+
+
+
+
+# a =(input("Введите свое имя   ")) 
+# print(a)      
+# str = 'kep'                          
+# reversed=''.join(reversed(str))
+# print(str)
+
+
+# print(reversed)
+
+
+#Равно:a == b
+#Не равно:a != b
+#Меньше, чем:a < b
+#Меньше или равно:a < = b
+#Больше, чем:a > b
+#Больше или равно:a > = b
+
+
+
+
+# password = (input("введите пароль"))
+
+# if password == "1234":
+
+#     print ("правильно ")
+
+# else:
+#        print("не правильно")
+ 
+ 
+
+
+
+# i = 13
+# while  i == 13:
+#     ans2 = input('ПИШИ ПАРОЛЬ')
+#     passs = 'beck'
+#     if ans2 == passs: 
+#         print('ВОШЛИ В СИСТЕМУ')
+#         break
+#     else:
+#         print('НЕ ПРАВИЛЬНО')
+
+
+
+
+
+
+
+# for i in range (0,0):
+   
+#    print  (i+1,  )')'
+
+
+
+
+
+
+# txt = "Hello world"
+
+# x = txt.upper()
+
+# print(x)
+
+
+
+# if__ name __== '_main_':
+
+# input = 'ABC'
+
+# chars = list(inpu
+
+
+
+
+
+
+# if __name__ == '__main__':
+ 
+#     input = 'ABC'
+ 
+#     chars = list(input)
+#     print(chars)
+
+
+
+
+
+
+# for i in range (0,100):
+   
+#     print  (i+1,  '')
+
+
+
+
+
+# логин = 'jack'
+# password = 9213
+# s =(input('ввдите логин:'))
+# if s ==логин:
+#     printut('введите пароль:'))
+# if a ==password :
+#     print("пароль верно ")
+# else:
+#     print("неверный пароль  ")
+# if s <= логин  :
+#     print("введите логин ещё раз")
+# else :
+#     a == password
+#     print("попробуйте ещё раз :")
+# ('логин верно')
+# else  :
+#     print("логин верно")
+# а = int(inp
+
+
+
+# int 12345678
+# float 22.3
+# complex 44j
+# bool = True,False
+# str 'одинарный'  "двойной"   """тройной"""
+# list=[],dict={},tuple=(),set{},
+# input суроо беру
+
+
+
+# Квадратный скобка менен любой созду кошо алабыз пример (строки, картежи,элемент бош болсо деле болот любой объект ар кандай типте
+# Принт квадратный скобка
+# Len("")- сколько объектов 
+# append("")- добавлять конец списка 
+# insert-(индекс, "сам объект")
+# Pop-(-2) удаляет и возвращает если не чего не передать то он удаляет последний объект 
+# extend- ["расширяет" "список" "добавляет"]конец 
+# remove-("удаляет")
+# count -("сколько")
+# index -("какой ряд")
+
+
+# list = ['1', '2', '3', '4', '5', "orange"] 
+# list.insert(4 , "apple")
+# if "orange" in list:
+
+   
+#     print("Orange element is not in list")
+# else:
+#     list.append("orange")
+#     print(list)
+
+
+# a = input()
+# while (a!='конец'):
+#     print(a)
+
+
+
+# a = ['akkdj' , 'pop', 'konec' , 'jack' , 'kep', ]
+# for 
+
+
+
+
+# a = int(input("san: "))
+# while a!=5: 
+# n hbtfgdgs
+
+
+
+# for i in range (1,30):
+#     if i == 15:
+#         continue
+#     print(i)
+
+
+# S = 1
+# while S <30:
+#     S += 1
+#     if S == 15:
+#         continue 
+#     print(S)
+
+
+# a = float(input("san jaz: "))
+# b = float(input("san jaz: "))
+
+
+
+
+
+# k = float(input("иттин жашы: "))
+# b =10.5
+# s =1
+# if a<=2:
+#     v =10.5
+#     s =a*b 
+
+
+
+
+# a = float(input("san jaz: "))
+# if a<=2:
+#     print(a*10.5)
+# else:
+#     print(21+(a-2)*4)
+
+
+
+
+a = str(input("напиши слово: "))
+if "blue" in a:
+    print("yes")
+else:
+    print("no")
